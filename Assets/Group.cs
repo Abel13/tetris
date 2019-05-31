4e78062307b079f24d46568f768ea5723f00ecd2
abel <abel.o.d@outlook.com>
2019-05-31 13:04:46 -0300

    first commit

---
+++ Assets/Group.cs
@@ -0,0 +1,159 @@
+﻿//using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Group : MonoBehaviour
+{
+    // Time since last gravity tick
+    float lastFall = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Default position not valid? Then it's game over
+        if (!IsValidGridPos())
+        {
+            Debug.Log("GAME OVER");
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+                // Its valid. Update grid.
+                updateGrid();
+            else
+                // Its not valid. revert.
+                transform.position += new Vector3(1, 0, 0);
+        }
+        // Move Right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(-1, 0, 0);
+        }
+        // Rotate
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, -90);
+
+            // See if valid
+            if (IsValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.Rotate(0, 0, 90);
+        }
+        // Fall
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+            }
+            else
+            {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.DeleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Spawner>().SpawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+        }
+        // Move Downwards and Fall
+        else if (Input.GetKeyDown(KeyCode.DownArrow) ||
+                 Time.time - lastFall >= 1)
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (IsValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+            }
+            else
+            {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.DeleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Spawner>().SpawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+
+            lastFall = Time.time;
+        }
+    }
+
+    bool IsValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+
+            // Not inside Border?
+            if (!Grid.InsideBorder(v))
+                return false;
+
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    void updateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.h; ++y)
+            for (int x = 0; x < Grid.w; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Groups
+    public GameObject[] groups;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnNext()
+    {
+        // Random Index
+        int i = Random.Range(0, groups.Length);
+
+        // Spawn Group at current Position
+        Instantiate(groups[i],
+                    transform.position,
+                    Quaternion.identity);
+    }
+}
